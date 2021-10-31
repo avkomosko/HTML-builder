4814e2813e3060f89551493df29144196696875a
Aliaksei <avkomosko@gmail.com>
2021-10-31 17:48:45 +0300

    feat: implement files in directoty function

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs').promises;
+const path = require('path');
+const dirPath = path.join(__dirname, 'secret-folder');
+
+async function listDirectory() {
+  try {
+    const files = await fs.readdir(dirPath, {withFileTypes: true});
+    for (const file of files) {
+      let isFile = file.isFile();
+      if (isFile) {
+        let filePath = path.join(dirPath, file.name);
+        let fileExt = path.extname(filePath);
+        let filename = path.basename(filePath, fileExt);
+        let stat = fs.stat(filePath);
+        let fileSize = (Math.trunc(((await stat).size / 1024) * 1000) / 1000 );
+        console.log(`${filename} - ${fileExt.slice(1)} - ${fileSize}kb`);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+}
+
+listDirectory();
