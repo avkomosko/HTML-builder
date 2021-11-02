48ad9310e16ba4adc4ba2cda59d60580dac17923
Aliaksei <avkomosko@gmail.com>
2021-11-02 13:03:03 +0300

    refactor: change merge styles function for future export

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,55 +1,42 @@
 const fs = require('fs').promises;
 const path = require('path');
-const stylePath = path.join(__dirname, 'styles');
-const dataArr = [];
-const outputPath = path.join(__dirname, 'project-dist', 'bundle.css');
 
-async function bundleStyles() {
+async function bundleStyles(stylesSourse, stylesOutput) {
+  const stylePath = path.join(__dirname, stylesSourse);
+  const dataArr = [];
+  const outputPath = path.join(__dirname, 'project-dist', stylesOutput);
+
   try {
     const files = await fs.readdir(stylePath, { withFileTypes: true });
-    for (const file of files) {
+    for (let file of files) {
       let isFile = file.isFile();
       if (isFile) {
         let filePath = path.join(stylePath, file.name);
         let fileExt = path.extname(filePath);
         if (fileExt.slice(1) === 'css') {
-          await readFile(filePath, 'utf-8');
+          const data = await fs.readFile(filePath, 'utf-8');
+          dataArr.push(data);
         }
       }
     }
+    const allData = dataArr.join('\n');
+    updateStyles(allData);
   } catch (err) {
     console.error(err);
   }
 
-  const allData = await dataArr.join('');
-
-  try {
-    await fs.unlink(outputPath);
-    
-  } catch {
-    appendFile(outputPath, allData);
-   
-  } finally {
-    appendFile(outputPath, allData);
-  }
-  
-
-  async function readFile(fileName) {
+  async function updateStyles(data) {
     try {
-      const data = await fs.readFile(fileName, 'utf-8');
-      dataArr.push(data);
-    } catch (err) {
-      throw err;
+      await fs.unlink(outputPath);
+      appendFile(outputPath, data);
+    } catch {
+      appendFile(outputPath, data);
     }
   }
 
   async function appendFile(fileName, data) {
-    try {
-      await fs.appendFile(fileName, data, 'utf-8');
-    } catch (err) {
-      throw err;
-    }
+    await fs.appendFile(fileName, data, 'utf-8');
   }
 }
 
-bundleStyles();
+bundleStyles('styles', 'bundle.css');
