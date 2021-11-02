395ea2608a4ff7546055c017ede1031135f3f315
Aliaksei <avkomosko@gmail.com>
2021-11-03 00:09:54 +0300

    feat: add index.html creation

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -10,6 +10,9 @@ const assetsPath = path.join(__dirname, 'assets');
 const assetsCopyPath = path.join(distPath, 'assets');
 
 async function buildPage() {
+  let htmlTemplate = '';
+  let components = {};
+
   async function makeDir(path) {
     try {
       await fs.stat(path);
@@ -26,10 +29,75 @@ async function buildPage() {
     }
   }
 
+  async function clearDir(dirPath) {
+    try {
+      const files = await fs.readdir(dirPath, { withFileTypes: true });
+      for (let file of files) {
+        let isFile = file.isFile();
+        if (isFile) {
+          let filePath = path.join(dirPath, file.name);
+          await fs.unlink(filePath);
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function readHtmltemp(filePath) {
+    const data = await fs.readFile(filePath, 'utf-8');
+    htmlTemplate += data;
+  }
+
+  async function getHtmlComponents(dirPath) {
+    const files = await fs.readdir(dirPath, { withFileTypes: true });
+    for (let file of files) {
+      let isFile = file.isFile();
+      if (isFile) {
+        let filePath = path.join(dirPath, file.name);
+        let fileExt = path.extname(filePath);
+        let filename = path.basename(filePath, fileExt);
+        if (fileExt === '.html') {
+          let data = await fs.readFile(filePath, 'utf-8');
+          components[filename] = data;
+        }
+      }
+    }
+  }
+  
+  async function createHtml() {
+    await readHtmltemp(path.join(__dirname, 'template.html'));
+    await getHtmlComponents(path.join(__dirname, 'components'));
+    let items = Object.keys(components);
+    for (let item of items) {
+      if (htmlTemplate.includes(item)) {
+        do {
+          htmlTemplate = htmlTemplate.replace(`{{${item}}}`, components[item]);
+        } while (htmlTemplate.indexOf(item) === -1);
+      }
+    }
+  }
+
+  async function writeHtml() {
+    await createHtml();
+    let fileName = path.join(distPath, 'index.html');
+    try {
+      await fs.stat(fileName);
+      await fs.unlink(fileName);
+    } catch (err) {
+      await fs.appendFile(fileName, '', 'utf-8');
+      return;
+    } finally {
+      await fs.appendFile(fileName, htmlTemplate, 'utf-8');
+    }
+  }
+  
   try {
     await makeDir(distPath);
     bundleStyles(stylePath, outputStyles);
+    writeHtml();
     await makeDir(assetsCopyPath);
+    await clearDir(assetsCopyPath);
     const files = await fs.readdir(assetsPath, { withFileTypes: true });
     for (let file of files) {
       let isFile = file.isFile();
