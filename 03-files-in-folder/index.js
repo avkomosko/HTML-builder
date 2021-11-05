c0432881f42bc43f7a2ce1b5131154d4896dbb1a
Aliaksei <avkomosko@gmail.com>
2021-11-05 23:22:44 +0300

    fix: add units to file size

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,20 +4,19 @@ const dirPath = path.join(__dirname, 'secret-folder');
 
 async function listDirectory() {
   try {
-    const files = await fs.readdir(dirPath, {withFileTypes: true});
+    const files = await fs.readdir(dirPath, { withFileTypes: true });
     for (const file of files) {
       if (file.isFile()) {
         let filePath = path.join(dirPath, file.name);
         let fileExt = path.extname(filePath);
         let filename = path.basename(filePath, fileExt);
         let stat = await fs.stat(filePath);
-        console.log(`${filename} - ${fileExt.slice(1)} - ${stat.size}`);
+        console.log(`${filename} - ${fileExt.slice(1)} - ${stat.size}b`);
       }
     }
   } catch (err) {
     console.error(err);
   }
-
 }
 
 listDirectory();
